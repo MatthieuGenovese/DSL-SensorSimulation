8a6bfe44a926c0fcb7facee6ab49dab068eb3cbf
MatthieuGenovese <gm102445@etu.unice.fr>
2018-01-31 15:56:28 +0100

    Merci git

---
+++ kernel/src/main/java/laws/States.java
@@ -0,0 +1,27 @@
+package laws;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Michael on 30/01/2018.
+ */
+public class States {
+    private ArrayList<String> value;
+
+    public States(ArrayList<String> states) {
+        this.value = states;
+    }
+
+
+    public ArrayList<String> getValue() {
+        return value;
+    }
+
+    public String getValue(int i){
+        return value.get(i);
+    }
+
+    public void setValue(ArrayList<String> states) {
+        this.value = states;
+    }
+}
